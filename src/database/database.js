18edc542b5efe96fa99c615c6e940ade4bb17424
Glitch (earthy-ambitious-sesame) <none>
2020-07-30 07:47:19 +0000

    🗾🎮 Checkpoint ./src/database/database.js:17652135/14201 ./package.json:17652135/35

--- src/database/database.js
+++ src/database/database.js
@@ -1,114 +1,121 @@
-const { Sequelize } = require('sequelize');
-const { Logger } = require('../utils/');
-
+const { Sequelize, DataTypes } = require("sequelize");
+const { Logger } = require("../utils/");
+require("dotenv").config();
 function Debug(...args) {
-    Logger.debug(args[0], 'Database');
+  Logger.debug(args[0], "Database");
 }
 
 const UnicronDB = new Sequelize(
-    'database',
-    process.env.UNICRON_DATABASE_USERNAME,
-    process.env.UNICRON_DATABASE_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'sqlite',
-        logging: Debug,
-        storage: `./database/${process.env.UNICRON_DATABASE_FILE}.sqlite`,
-        retry: {
-            max: 10,
-        },
-        transactionType: 'IMMEDIATE',
-    }
+  "database",
+  process.env.UNICRON_DATABASE_USERNAME,
+  process.env.UNICRON_DATABASE_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "sqlite",
+    logging: Debug,
+    storage: `./database/bruhhh.sqlite`,
+    retry: {
+      max: 10
+    },
+    transactionType: "IMMEDIATE"
+  }
 );
 const UserDB = new Sequelize(
-    'database',
-    process.env.USER_DATABASE_USERNAME,
-    process.env.USER_DATABASE_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'sqlite',
-        logging: Debug,
-        storage: `./database/${process.env.USER_DATABASE_FILE}.sqlite`,
-        retry: {
-            max: 10,
-        },
-        transactionType: 'IMMEDIATE',
-    }
-)
+  "database",
+  process.env.USER_DATABASE_USERNAME,
+  process.env.USER_DATABASE_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "sqlite",
+    logging: Debug,
+    storage: `./database/hellooo.sqlite`,
+    retry: {
+      max: 10
+    },
+    transactionType: "IMMEDIATE"
+  }
+);
 const GuildDB = new Sequelize(
-    'database',
-    process.env.GUILD_DATABASE_USERNAME,
-    process.env.GUILD_DATABASE_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'sqlite',
-        logging: Debug,
-        storage: `./database/${process.env.GUILD_DATABASE_FILE}.sqlite`,
-        retry: {
-            max: 10,
-        },
-        transactionType: 'IMMEDIATE',
-    }
-)
+  "database",
+  process.env.GUILD_DATABASE_USERNAME,
+  process.env.GUILD_DATABASE_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "sqlite",
+    logging: Debug,
+    storage: `./database/thisshouldwork.sqlite`,
+    retry: {
+      max: 10
+    },
+    transactionType: "IMMEDIATE"
+  }
+);
 
-const Admin = require('../models/unicron')(UnicronDB, );
+const Admin = require("../models/unicron")(UnicronDB, DataTypes);
 
-const UserProfile = UserDB.import('../models/user/profile');
-const UserInventory = UserDB.import('../models/user/inventory');
+const UserProfile = require("../models/user/profile")(UserDB, DataTypes);
+const UserInventory = require("../models/user/inventory")(UserDB, DataTypes);
 
-const GuildSettings = GuildDB.import('../models/guild/settings');
-const GuildModeration = GuildDB.import('../models/guild/moderation');
-const GuildWelcome = GuildDB.import('../models/guild/welcome');
-const GuildLeave = GuildDB.import('../models/guild/leave');
-const GuildFilter = GuildDB.import('../models/guild/filter');
-const GuildTags = GuildDB.import('../models/guild/tags');
-const GuildVerification = GuildDB.import('../models/guild/verification');
-const GuildTicket = GuildDB.import('../models/guild/ticket');
-const GuildDynamicVoice = GuildDB.import('../models/guild/dynamicVoice');
-const GuildMember = GuildDB.import('../models/guild/member');
+const GuildSettings = require("../models/guild/settings")(GuildDB, DataTypes);
+const GuildModeration = require("../models/guild/moderation")(
+  GuildDB,
+  DataTypes
+);
+const GuildWelcome = require("../models/guild/welcome")(GuildDB, DataTypes);
+const GuildLeave = require("../models/guild/leave")(GuildDB, DataTypes);
+const GuildFilter = require("../models/guild/filter")(GuildDB, DataTypes);
+const GuildTags = require("../models/guild/tags")(GuildDB, DataTypes);
+const GuildVerification = require("../models/guild/verification")(
+  GuildDB,
+  DataTypes
+);
+const GuildTicket = require("../models/guild/ticket")(GuildDB, DataTypes);
+const GuildDynamicVoice = require("../models/guild/dynamicVoice")(
+  GuildDB,
+  DataTypes
+);
+const GuildMember = require("../models/guild/member")(GuildDB, DataTypes);
 
-(async function () {
-    if (process.argv.includes('--database')) {
-        const force = process.argv.includes('--reset');
-        Logger.info('Connecting to databases...');
-        if (process.argv.includes('--all') || process.argv.includes('--unicron')) {
-            await UnicronDB.sync({ force }).then(() => {
-                Logger.info('Unicron Database Synced!');
-                UnicronDB.close();
-            }).catch(Logger.error);
-        }
-        if (process.argv.includes('--all') || process.argv.includes('--user')) {
-            await UserDB.sync({ force }).then(() => {
-                Logger.info('User Database Synced!');
-                UserDB.close();
-            }).catch(Logger.error);
-        }
-        if (process.argv.includes('--all') || process.argv.includes('--guild')) {
-            await GuildDB.sync({ force }).then(() => {
-                Logger.info('Guild Database Synced!');
-                GuildDB.close();
-            }).catch(Logger.error);
-        }
-        process.exit(0);
-    }
+(async function() {
+  if (process.argv.includes("--database")) {
+    Logger.info("Connecting to databases...");
+    await UnicronDB.sync({ force: true, logging: console.log })
+      .then(() => {
+        Logger.info("Unicron Database Synced!");
+        UnicronDB.close();
+      })
+      .catch(Logger.error);
+    await UserDB.sync({ force: true, logging: console.log })
+      .then(() => {
+        Logger.info("User Database Synced!");
+        UserDB.close();
+      })
+      .catch(Logger.error);
+    await GuildDB.sync({ force: true, logging: console.log })
+      .then(() => {
+        Logger.info("Guild Database Synced!");
+        GuildDB.close();
+      })
+      .catch(Logger.error);
+    process.exit(0);
+  }
 })();
 
-
 module.exports = {
-    UnicronDB,
-    GuildDB,
-    UserDB,
-    Admin,
-    UserProfile,
-    UserInventory,
-    GuildSettings,
-    GuildDynamicVoice,
-    GuildFilter,
-    GuildWelcome,
-    GuildLeave,
-    GuildMember,
-    GuildModeration,
-    GuildTags,
-    GuildTicket,
-    GuildVerification,
-}
+  UnicronDB,
+  GuildDB,
+  UserDB,
+  Admin,
+  UserProfile,
+  UserInventory,
+  GuildSettings,
+  GuildDynamicVoice,
+  GuildFilter,
+  GuildWelcome,
+  GuildLeave,
+  GuildMember,
+  GuildModeration,
+  GuildTags,
+  GuildTicket,
+  GuildVerification
+};
