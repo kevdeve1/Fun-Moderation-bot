7f66e97fa1e91b9754800c0112ae034c0ef347f7
Glitch (earthy-ambitious-sesame) <none>
2020-11-26 11:24:36 +0000

    🎯💽 Checkpoint ./src/commands/utility/prefix.js:24811736/14

--- src/commands/utility/prefix.js
+++ src/commands/utility/prefix.js
@@ -31,7 +31,7 @@ module.exports = class extends BaseCommand {
             .setColor(0x00FFFF)
             .setDescription(`
             My Prefix for this Server/Guild is \`${prefix}\` or you can just ping me as the prefix.\n
-            You can change my prefix using \`${prefix}set prefix [prefix]\` but you need permission level \`Server Administrator\`.`
+            You can change my prefix using \`${prefix}config set prefix [prefix]\` but you need permission level \`Server Administrator\`.`
             )
         );
     }
