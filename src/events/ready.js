08fbed75cdcaa9aa49a9d52d324b7dc7d44b4993
Glitch (earthy-ambitious-sesame) <none>
2021-05-31 03:36:15 +0000

    ⛲️🐆 Checkpoint ./src/events/ready.js:24811736/350

--- src/events/ready.js
+++ src/events/ready.js
@@ -18,4 +18,5 @@ module.exports = (client) => {
   console.log(
     `Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
   );
+ 
 };
