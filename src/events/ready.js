1b19c5906e014eea2109230a04051c7b4a084c96
Glitch (earthy-ambitious-sesame) <none>
2021-05-31 02:38:16 +0000

    💫☃️ Checkpoint ./server.js:24811736/734 ./src/events/ready.js:24811736/646

--- src/events/ready.js
+++ src/events/ready.js
@@ -18,11 +18,28 @@ module.exports = (client) => {
   console.log(
     `Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
   );
-  const activities = [`unicorn-bot in ${client.guilds.cache.size} guilds`,"!help",`over ${client.users.cache.size} users!`];
-  setInterval(() => {
-    let activity = activities[Math.floor(Math.random() * activities.length)];
-    client.user.setActivity("!help", { type: "PLAYING" });
-  }, 20000);
-};
+  client.on("ready", () => {
 
+var word = ["'266 Members', { type: 'WATCHING'}", "'6 Servers', {type: 'WATCHING'}"];
 
+let i = 0;
+
+client.user.setActivity(word[0]);
+
+i++;
+
+setInterval(() => {
+
+ if (i >= word.length + 1) {
+
+   i = 0;
+
+ }
+
+client.user.setActivity(word[i]);
+
+i++;
+
+}, 1000 * 15); // 30 = 30s || 1000 * 1 = 1s
+
+});
