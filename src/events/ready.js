e18cd3e858e54fae529c4562217ce6e79dfc872b
Glitch (earthy-ambitious-sesame) <none>
2021-05-31 03:25:11 +0000

    🐔🌖 Checkpoint ./src/events/ready.js:24811736/329

--- src/events/ready.js
+++ src/events/ready.js
@@ -18,28 +18,4 @@ module.exports = (client) => {
   console.log(
     `Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
   );
-  client.on("ready", () => {
-
-var word = ["'266 Members', { type: 'WATCHING'}", "'6 Servers', {type: 'WATCHING'}"];
-
-let i = 0;
-
-client.user.setActivity(word[0]);
-
-i++;
-
-setInterval(() => {
-
- if (i >= word.length + 1) {
-
-   i = 0;
-
- }
-
-client.user.setActivity(word[i]);
-
-i++;
-
-}, 1000 * 15); // 30 = 30s || 1000 * 1 = 1s
-
-});
+};
